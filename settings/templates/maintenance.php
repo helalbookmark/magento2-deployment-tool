f92de948799e7e02cefbbe62d99c5d7df23e6b8c
alojua <juan.alonso@staempfli.com>
2017-02-26 03:28:39 +0100

    Work in progress

---
+++ settings/templates/maintenance.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+    body { text-align: center; padding: 10px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; margin: 0 auto; }
+    a { color: #dc8100; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. <br/>If you need to you can always contact us, otherwise we&rsquo;ll be back online shortly!</p>
+    </div>
+</article>
